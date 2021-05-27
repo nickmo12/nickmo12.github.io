e03758783c5abb566256e71e9d55fb6e03e44540
nmorales <nickmorales12@gmail.com>
2021-05-26 20:54:37 -0400

    Added react router dom

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
 import logo from './Nick.PNG';
 import './App.css';
-import Home from './components/Home'
+//import Home from './components/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Home/>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My name is Nick and I'm going to master React!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/nickmo12"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Check Me Out!
-        </a> */}
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Me</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Me</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/contact">
+              <ContactMe />
+            </Route>
+          <Route path="/about">
+            <AboutMe />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function AboutMe() {
+  return <h2>About Me</h2>;
+}
+
+function Projects() {
+  return <h2>Projects</h2>;
+}
+
+function ContactMe() {
+  return <h2>Contact Me</h2>
+}
